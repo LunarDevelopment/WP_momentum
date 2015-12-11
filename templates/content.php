9563b619c26326cf192d9d7c9078054408509736
Lewis Dimmick <development.lunar@gmail.com>
2015-12-11 08:46:24 +0000

    further work on home page

--- templates/content.php
+++ templates/content.php
@@ -1,9 +1,17 @@
 <article <?php post_class(); ?>>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
-  <div class="entry-summary">
+  <div class="row">
+  <div class="col-md-3 sub-header text-right entry-summary">
     <?php the_excerpt(); ?>
+  </div>
+  <div class="col-md-6 twoColumns entry-summary">
+    <?php the_excerpt(); ?>
+  </div>
+  <div class="col-md-3">
+    <?php  ?>
+  </div>
   </div>
 </article>
